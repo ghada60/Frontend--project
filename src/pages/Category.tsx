5cb07a166d5a78283bbb4c05dceefb56f0091e16
ghada <ghada.alnmla98@gmail.com>
2024-01-02 19:09:40 +0300

    fix xx

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,28 +1,5 @@
-// import { useEffect } from 'react'
-
-// import { getCategoryThunk } from '../redux/slices/category/categorySlice'
-// import { AppDispatch, RootState } from '../redux/store'
-// import { useDispatch, useSelector } from 'react-redux'
-
-// export function Category() {
-//   const categories = useSelector((state: RootState) => state.categories)
-//   const dispatch = useDispatch<AppDispatch>()
-
-//   useEffect(() => {
-//     dispatch(getCategoryThunk())
-//   }, [])
-
-//   return (
-//     <div>
-//       {categories.items.map((category) => (
-//         <div key={category.categoryName}>
-//           <h1>{category.categoryName}</h1>
-//         </div>
-//       ))}
-//     </div>
-//   )
-// }
 import { useEffect } from 'react'
+
 import { getCategoryThunk } from '../redux/slices/category/categorySlice'
 import { AppDispatch, RootState } from '../redux/store'
 import { useDispatch, useSelector } from 'react-redux'
@@ -33,19 +10,15 @@ export function Category() {
 
   useEffect(() => {
     dispatch(getCategoryThunk())
-  }, [dispatch])
+  }, [])
 
   return (
     <div>
-      {categories.items && categories.items.length > 0 ? (
-        categories.items.map((category) => (
-          <div key={category.categoryName}>
-            <h1>{category.categoryName}</h1>
-          </div>
-        ))
-      ) : (
-        <p>No categories found</p>
-      )}
+      {categories.items.map((category) => (
+        <div key={category.categoryName}>
+          <h1>{category.categoryName}</h1>
+        </div>
+      ))}
     </div>
   )
 }
